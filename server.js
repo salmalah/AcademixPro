8ea14c18357806db325de2fff625d29da83410a9
Salma <s.salmalahmidi@gmail.com>
2024-06-05 17:23:14 +0100

    Imported and called the connection setup function in server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const PORT = 5000;
-const dbURI = "mongodb://localhost:27017/Academix-Pro";
-mongoose
-  .connect(dbURI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+const connectDB = require("./config/db");
+connectDB();
 app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
