39984d082c8def814a99a74525a399d347a67c69
Salma <s.salmalahmidi@gmail.com>
2024-06-05 17:16:53 +0100

    update server.js file and Configured MongoDB connection using Mongoose

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 5000;
+const dbURI = "mongodb://localhost:27017/Academix-Pro";
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
