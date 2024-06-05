8e2f8d23df04a4d820c78c192f4e8e5f1ee0b86c
Salma <s.salmalahmidi@gmail.com>
2024-06-05 17:52:23 +0100

    Integrated user routes into the server to handle user-related endpoints

--- server.js
+++ server.js
@@ -2,5 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const connectDB = require("./config/db");
+
+// Import routes
+const userRoutes = require("./routes/userRoutes");
+
+// Mount routes
+app.use("/api/users", userRoutes);
+
 connectDB();
 app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
