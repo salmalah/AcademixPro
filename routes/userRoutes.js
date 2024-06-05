6611bc8cde50f0233e5780907cf0b34da0301639
Salma <s.salmalahmidi@gmail.com>
2024-06-05 18:20:22 +0100

    Created a route for user signup in the user routes to handle user registration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,5 @@ const userController = require("../controllers/userController");
 router.get("/login", (req, res) => {
   res.send({ msg: "This is login page test" });
 });
+router.post("/signup", userController.signUp);
 module.exports = router;
