c44df050e48087edc8739863da4daedde6e64c85
Salma <s.salmalahmidi@gmail.com>
2024-06-06 14:10:48 +0100

    Update package-lock.json and package.json dependencies and server added Middleware

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
 const PORT = 5000;
 const connectDB = require("./config/db");
 
 // Import routes
 const userRoutes = require("./routes/userRoutes");
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Mount routes
 app.use("/api/users", userRoutes);
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 connectDB();
 app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
